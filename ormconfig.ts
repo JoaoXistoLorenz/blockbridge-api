dd1e9474c49d92de371734637f7309f7f2ed1bac
joao.vitor <joao.vitor@tn3.com.br>
2024-09-09 15:47:50 -0300

    adicionando .env

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,15 +8,17 @@ import { TipoMenu } from './src/tipo-menu/tipo-menu.entity';
 import { Usuario } from './src/usuario/usuario.entity';
 import { Link } from './src/link/link.entity';
 import { PlataformaBlockchain } from './src/plataforma-blockchain/plataforma-blockchain.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const connectionSource = new DataSource({
   migrationsTableName: 'migrations',
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'projeto',
+  type: process.env.TYPE,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   synchronize: true,
   entities: [Blockchain, Melhoria, Plataforma, TipoEscalabilidade, TipoMenu, Usuario, PlataformaBlockchain, Link],
   migrations: ['migrations/*.ts'],
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'projeto',
+  type: process.env.TYPE,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   utoLoadEntities: true,
