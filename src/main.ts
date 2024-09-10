3406c9b6e4e50b9e1124fdd500ec02d1c3b16275
joao.vitor <joao.vitor@tn3.com.br>
2024-09-10 17:39:00 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  /* const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000); */
-  const httpsOptions = {
-    key: fs.readFileSync('src/secrets/certificate.key'),
-    cert: fs.readFileSync('src/secrets/certificate.crt'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    httpsOptions,
-  });
-
-  await app.listen(3000);
+  if (process.env.AMBIENDE == 'prod') {
+    const app = await NestFactory.create(AppModule, { cors: true });
+    await app.listen(3000);
+  } else {
+    const httpsOptions = {
+      key: fs.readFileSync('src/secrets/certificate.key'),
+      cert: fs.readFileSync('src/secrets/certificate.crt'),
+    };
+    const app = await NestFactory.create(AppModule, {
+      cors: true,
+      httpsOptions,
+    });
+    await app.listen(3000);
+  }
 }
 bootstrap();
